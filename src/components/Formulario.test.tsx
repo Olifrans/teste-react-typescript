5d55a7196418a5f24d36920486040608c27c74cb
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-09 12:31:40 -0300

    feat: test com react e typescript

---
+++ src/components/Formulario.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+//Jest
+
+test("quando o input está vazio, novos participantes não podem ser adcionados", () =>{
+
+    //econtrar no DOM o input
+    //econtrar botão
+
+    //garantir que o input esteja no documento
+    //garantir que o botão esteje desabilitado
+
+})
