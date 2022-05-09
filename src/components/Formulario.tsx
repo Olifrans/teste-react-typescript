e7a80167e295fd5ea3c48050f783e461dcc69546
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-09 13:14:46 -0300

    feat: Test Driven Developer

--- src/components/Formulario.tsx
+++ src/components/Formulario.tsx
@@ -4,8 +4,11 @@ const Formulario = () => {
       <input type="text" placeholder="Insira os nomes do participantes"/>
       {/* <input type="text" placeholder="Qualquer coisa"/> */}
 
+
       <button disabled={true}>Adicionar</button>
       {/* <button disabled={false}>Adicionar</button> */}
+
+
     </form>
   );
 };
